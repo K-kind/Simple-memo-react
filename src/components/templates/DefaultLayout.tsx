3c53de56401467a9fcfa14108c38a50e557945b8
Keita Katahira <katahirakeita1201@gmail.com>
2021-10-16 14:50:45 +0900

    Add DefaultLayout component

---
+++ src/components/templates/DefaultLayout.tsx
@@ -0,0 +1,32 @@
+import { VFC, ReactNode } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+type Props = {
+  children: ReactNode;
+};
+
+const DefaultLayout: VFC<Props> = ({ children }) => (
+  <>
+    <AppBar position="relative">
+      <Toolbar sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+        <Typography variant="h3" color="inherit" noWrap>
+          React
+        </Typography>
+      </Toolbar>
+    </AppBar>
+    <Container component="main" maxWidth="sm" sx={{ pt: 8, pb: 6 }}>
+      {children}
+    </Container>
+    <Box sx={{ p: 6 }} component="footer">
+      <Typography variant="body2" color="text.secondary" align="center">
+        Copyright © Learn React {new Date().getFullYear()}
+      </Typography>
+    </Box>
+  </>
+);
+
+export default DefaultLayout;
